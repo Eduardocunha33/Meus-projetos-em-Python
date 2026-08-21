b6997ed56cc80609dc2043f1a55becaf0f19eb9d
Eduardo Lima Cunha <limaeduardo008@gmail.com>
2026-08-21 11:30:34 -0300

    Teste

---
+++ teste.py
@@ -0,0 +1,12 @@
+class Person: 
+    def __init__(self, name, age, higth, weigth, gender):
+        self.name = name
+        self.age = age
+        self.higth = higth
+        self.weigth = weigth
+        self.gender = gender
+
+Jão = Person("João", 21, 1.80, 70, "Masculine")
+print(f"My name is {Jão.name}, i'm {Jão.age} years old, I weigh {Jão.weigth} kg end I am {Jão.higth} cm tall.")
+
+
