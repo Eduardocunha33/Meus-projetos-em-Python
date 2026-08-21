03a483608e380a1483cf49d758718c8ea701cceb
Eduardo Lima Cunha <limaeduardo008@gmail.com>
2026-08-21 11:38:22 -0300

    Atualização de dados

--- teste.py
+++ teste.py
@@ -9,3 +9,5 @@ class Person:
 Jão = Person("João", 21, 1.80, 70, "Masculine")
 print(f"My name is {Jão.name}, i'm {Jão.age} years old, I weigh {Jão.weigth} kg end I am {Jão.higth} cm tall.")
 
+Ana = Person("Ana", 23, 1.65, 56, "feminine")
+print(f"My name is {Ana.name}, i'm {Ana.age} years old, I weigh {Ana.weigth} kg end I am {Ana.higth} cm tall.")
